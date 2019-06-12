f32404800c999d4ffb0fed0b4becf0c5a985eb67
Frank Putnam, Jr <frankputnam@comcast.net>
2019-06-12 17:30:05 -0400

    Test of overloaded functions.
    overloaded set_element(), overloaded get_element()

--- Code/Odeyssey_matrix.cpp
+++ Code/Odeyssey_matrix.cpp
@@ -1,9 +1,9 @@
 /*******************************************************************************
-* Title                 :   OSR Matrix Class
-* Filename              :   OSR_matrix.h
+* Title                 :   OSR Matrix Class Unit Tests
+* Filename              :   Odyssey_matrix.cpp
 * Author                :   Frank Putnam, Jr.
 * Origin Date           :   2019-06-11
-* Version               :   1.0.0
+* Version               :   0.0.1
 * Compiler              :   Microsoft Visual Studio 2019 C30 v3.30c
 * Target                :   PC
 * Notes                 :   None
@@ -14,9 +14,10 @@
 *  2019-06-11       0.1              FRP      Created.
 *
 *******************************************************************************/
-/** @file XXX.c
- *  @brief This module contains the
- */
+/** @file Odeyssey_matrix.cpp
+ *  @brief This module contains the 'main' function. 
+ *  Program execution begins and ends there.
+*/
  /******************************************************************************
  * Includes
  *******************************************************************************/
@@ -27,7 +28,7 @@
  /**
   * Doxygen tag for documenting variables and constants
   */
-#define   CONSTANT					5
+constexpr auto CONSTANT = 5;
 
   /******************************************************************************
   * Module Preprocessor Macros
@@ -49,7 +50,7 @@
   * Function Definitions
   *******************************************************************************/
   /******************************************************************************
-  * Function : Pwm_Init()
+  * Function : main()
   */
   /*
   * @section Description Description:
@@ -62,23 +63,19 @@
   *
   * @section Example Example:
   * @code
-  *    Pwm_Init();
+  *    main();
   * @endcode
   *
-  * @see Pwm_Enable
   *
   *  ----------------------
   *  - HISTORY OF CHANGES -
   *  ----------------------
-  *    Date    Software Version    Initials   Description
-  *  XX/XX/XX    XXXXXXXXXXX         JWB      Function Created.
+  *    Date      Software Version    Initials   Description
+  *  2019/06/11         0.1            FRP      Function Created.
   *
   *******************************************************************************/
 
 
-/*************** END OF FUNCTIONS ***************************************************************************/
-// Odeyssey_matrix.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 
 #include <iostream>
@@ -86,10 +83,142 @@
 
 int main()
 {
-	OSR_matrix* A_matrix = new OSR_matrix(2,2,OSR_matrix::UINT_TYPE);
+	unsigned Row_count    = 0;
+	unsigned Column_count = 0;
+	unsigned Matrix_size  = 0;
 
-	std::cout << "Rows " << A_matrix->get_number_of_rows() << "   Columns " << A_matrix->get_number_of_columns() << '/n';
+	OSR_matrix* Byte_matrix = new OSR_matrix(2,2,OSR_matrix::BYTE_TYPE);
+
+	Row_count    = Byte_matrix->get_number_of_rows();
+	Column_count = Byte_matrix->get_number_of_columns();
+	Matrix_size  = Byte_matrix->get_size();
+
+	std::cout << "Byte Matrix   Rows " << Row_count << "   Columns " << Column_count << "   Size " << Matrix_size << "\n";
+
+	char A11b = 'A';
+	char A12b = 'B';
+	char A21b = 'C';
+	char A22b = 'D';
+
+	Byte_matrix->set_element(1, 1, A11b);
+	Byte_matrix->set_element(1, 2, A12b);
+	Byte_matrix->set_element(2, 1, A21b);
+	Byte_matrix->set_element(2, 2, A22b);
+
+	A11b = '0';
+	A12b = '0';
+	A21b = '0';
+	A22b = '0';
+
+	Byte_matrix->get_element(1, 1, &A11b); 
+	Byte_matrix->get_element(1, 2, &A12b);
+	Byte_matrix->get_element(2, 1, &A21b);
+	Byte_matrix->get_element(2, 2, &A22b);
+
+	std::cout << "   " << A11b << "   " << A12b << "\n" << "   " << A21b << "   " << A22b << "\n";
+	
+	
+	
+	
+	OSR_matrix* Unsigned_integer_matrix = new OSR_matrix(2, 2, OSR_matrix::UINT_TYPE);
+
+	Row_count    = Unsigned_integer_matrix->get_number_of_rows();
+	Column_count = Unsigned_integer_matrix->get_number_of_columns();
+	Matrix_size  = Unsigned_integer_matrix->get_size();
+
+	std::cout << "Unsigned Interger Matrix   Rows " << Row_count << "   Columns " << Column_count << "   Size " << Matrix_size << "\n";
+
+	unsigned A11u = 11u;
+	unsigned A12u = 12u;
+	unsigned A21u = 21u;
+	unsigned A22u = 22u;
+
+	Unsigned_integer_matrix->set_element(1, 1, A11u);
+	Unsigned_integer_matrix->set_element(1, 2, A12u);
+	Unsigned_integer_matrix->set_element(2, 1, A21u);
+	Unsigned_integer_matrix->set_element(2, 2, A22u);
+
+	A11u = 0u;
+	A12u = 0u;
+	A21u = 0u;
+	A22u = 0u;
+
+	Unsigned_integer_matrix->get_element(1, 1, &A11u);
+	Unsigned_integer_matrix->get_element(1, 2, &A12u);
+	Unsigned_integer_matrix->get_element(2, 1, &A21u);
+	Unsigned_integer_matrix->get_element(2, 2, &A22u);
+
+	std::cout << "   " << A11u << "   " << A12u << "\n" << "   " << A21u << "   " << A22u << "\n\n";
+
+
+	OSR_matrix* Signed_integer_matrix = new OSR_matrix(2, 2, OSR_matrix::SINT_TYPE);
+
+	Row_count    = Signed_integer_matrix->get_number_of_rows();
+	Column_count = Signed_integer_matrix->get_number_of_columns();
+	Matrix_size  = Signed_integer_matrix->get_size();
+
+	std::cout << "Signed Integer Matrix   Rows " << Row_count << "   Columns " << Column_count << "   Size " << Matrix_size << "\n";
+
+	int A11i =  11;
+	int A12i = -12;
+	int A21i = -21;
+	int A22i =  22;
+
+	Signed_integer_matrix->set_element(1, 1, A11i);
+	Signed_integer_matrix->set_element(1, 2, A12i);
+	Signed_integer_matrix->set_element(2, 1, A21i);
+	Signed_integer_matrix->set_element(2, 2, A22i);
+
+	A11i = 0;
+	A12i = 0;
+	A21i = 0;
+	A22i = 0;
+
+	Signed_integer_matrix->get_element(1, 1, &A11i);
+	Signed_integer_matrix->get_element(1, 2, &A12i);
+	Signed_integer_matrix->get_element(2, 1, &A21i);
+	Signed_integer_matrix->get_element(2, 2, &A22i);
+
+	std::cout << "   " << A11i << "   " << A12i << "\n" << "   " << A21i << "   " << A22i << "\n\n";
+
+
+
+	OSR_matrix* Float_matrix = new OSR_matrix(2, 2, OSR_matrix::FLOAT_TYPE);
+
+	Row_count    = Float_matrix->get_number_of_rows();
+	Column_count = Float_matrix->get_number_of_columns();
+	Matrix_size  = Float_matrix->get_size();
+
+	std::cout << "Float Matrix   Rows " << Row_count << "   Columns " << Column_count << "   Size " << Matrix_size << "\n";
+
+	float A11f = -1.1;
+	float A12f =  1.2;
+	float A21f =  2.1;
+	float A22f = -2.2;
+
+	Float_matrix->set_element(1, 1, A11f);
+	Float_matrix->set_element(1, 2, A12f);
+	Float_matrix->set_element(2, 1, A21f);
+	Float_matrix->set_element(2, 2, A22f);
+
+	A11f = 0.0;
+	A12f = 0.0;
+	A21f = 0.0;
+	A22f = 0.0;
+
+	Float_matrix->get_element(1, 1, &A11f);
+	Float_matrix->get_element(1, 2, &A12f);
+	Float_matrix->get_element(2, 1, &A21f);
+	Float_matrix->get_element(2, 2, &A22f);
+
+	std::cout << "   " << A11f << "   " << A12f << "\n" << "   " << A21f << "   " << A22f << "\n\n";
+
+
 }
+
+/*************** END OF FUNCTIONS ***************************************************************************/
+
+
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
