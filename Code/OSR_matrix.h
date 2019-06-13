a290cfc642b50c5c768cf8dad08adb079b0d5101
Frank Putnam, Jr <frankputnam@comcast.net>
2019-06-13 17:55:40 -0400

    Overloaded functions and operators
    Reworked overloaded get_element, overloaded set_element.
    Implemented overloaded operator=, overloaded operator+=, overloaded+.

--- Code/OSR_matrix.h
+++ Code/OSR_matrix.h
@@ -3,7 +3,7 @@
 * Filename              :   OSR_matrix.h
 * Author                :   Frank Putnam, Jr.
 * Origin Date           :   2019-06-08
-* Version               :   1.0.0
+* Version               :   0.0.3
 * Compiler              :   Microsoft Visual Studio 2019 C30 v3.30c
 * Target                :   PC
 * Notes                 :   None
@@ -12,6 +12,9 @@
 *
 *    Date      Software Version    Initials   Description
 *  2019-06-11       0.1              FRP      Created.
+*  2019/06/12       0.2              FRP      Implemented constructor, destructor, overloaded get_element, overloaded set_element.
+*  2019/06/13       0.3              FRP      Reworked overloaded get_element, overloaded set_element.
+*                                             Implemented overloaded operator=, overloaded operator+=, overloaded+.
 *
 *****************************************************************************/
 /** @file OSR_matrix.h
@@ -78,191 +81,75 @@ public:
 		: _Number_of_rows{ initial_number_of_rows }, _Number_of_columns{ initial_number_of_columns }, _Matrix_elements_type{ initial_matrix_type }
 
 	{
-		_Matrix_size = initial_number_of_rows * initial_number_of_columns;
+		_Matrix_size_in_bytes = initial_number_of_rows * initial_number_of_columns;
 			
 		switch (_Matrix_elements_type)
 		{
 			case BYTE_TYPE :
 			{
-				_Matrix_size *= sizeof(char); 
-				Matrix_array = new char[_Matrix_size];
+				_Matrix_size_in_bytes *= sizeof(char);
+				Matrix_array = new char[_Matrix_size_in_bytes];
 				break;
 			}
 			
 			case UINT_TYPE :
 			{
-				_Matrix_size *= sizeof(unsigned);
-				Matrix_array = new unsigned[_Matrix_size];
+				_Matrix_size_in_bytes *= sizeof(unsigned);
+				Matrix_array = new unsigned[_Matrix_size_in_bytes];
 				break;
 			}
 			
 			case SINT_TYPE :
 			{
-				_Matrix_size *= sizeof(int);
-				Matrix_array = new int[_Matrix_size];
+				_Matrix_size_in_bytes *= sizeof(int);
+				Matrix_array = new int[_Matrix_size_in_bytes];
 				break;
 			}
 
 			case FLOAT_TYPE :
 			{
-				_Matrix_size *= sizeof(float);
-				Matrix_array = new float[_Matrix_size];
+				_Matrix_size_in_bytes *= sizeof(float);
+				Matrix_array = new float[_Matrix_size_in_bytes];
 				break;
 			}
 
 			default :
 			{
-				_Matrix_size = 0;
+				_Matrix_size_in_bytes = 0;
 				Matrix_array = nullptr;
 			}
 		}
 	}
 	
-	~OSR_matrix() { delete Matrix_array; }
+	~OSR_matrix() {}
 
 	unsigned get_number_of_rows(void)    { return _Number_of_rows; }
 	unsigned get_number_of_columns(void) { return _Number_of_columns; }
-	unsigned get_size(void) { return _Matrix_size; }
+	unsigned get_size_in_bytes(void)     { return _Matrix_size_in_bytes; }
 
 	OSR_matrix::Element_types get_type_of_element(void) { return _Matrix_elements_type; }
 
 	void Transpose(void);
 
-	void get_element(unsigned Row, unsigned Column, unsigned* Element)
-	{
-		unsigned _Position = 0;
-		unsigned* Location = nullptr;
-
-		if ((Row > 0) && (Row <= _Number_of_rows) && (Column > 0) && (Column <= _Number_of_columns))
-		{
-			_Position = (Row - 1) * _Number_of_columns;
-
-			_Position += (Column - 1);
-
-			Location = (unsigned*)(&Matrix_array + _Position);
-
-			*Element = *Location;
-		}
-	}
+	void get_element(unsigned Row, unsigned Column, unsigned* Element);
 	
-	void get_element(unsigned Row, unsigned Column, int* Element)
-	{
-		unsigned _Position = 0;
-		int* Location = nullptr;
+	void get_element(unsigned Row, unsigned Column, int* Element);
+	
+	void get_element(unsigned Row, unsigned Column, float* Element);
+	
+	void get_element(unsigned Row, unsigned Column, char* Element);
+	
+	void set_element(unsigned Row, unsigned Column, unsigned Data);
+	
+	void set_element(unsigned Row, unsigned Column, int Data);
+	
+	void set_element(unsigned Row, unsigned Column, float Data);
+	
+	void set_element(unsigned Row, unsigned Column, char Data);
 
-		if ((Row > 0) && (Row <= _Number_of_rows) && (Column > 0) && (Column <= _Number_of_columns))
-		{
-			_Position = (Row - 1) * _Number_of_columns;
-
-			_Position += (Column - 1);
-
-			Location = (int*)(&Matrix_array + _Position);
-
-			*Element = *Location;
-		}
-	}
-
-	void get_element(unsigned Row, unsigned Column, float* Element)
-	{
-		unsigned _Position = 0;
-		float* Location = nullptr;
-
-		if ((Row > 0) && (Row <= _Number_of_rows) && (Column > 0) && (Column <= _Number_of_columns))
-		{
-			_Position = (Row - 1) * _Number_of_columns;
-
-			_Position += (Column - 1);
-
-			Location = (float*)(&Matrix_array + _Position);
-
-			*Element = *Location;
-		}
-	}
-
-	void get_element(unsigned Row, unsigned Column, char* Element)
-	{
-		unsigned _Position = 0;
-		char* Location = nullptr;
-
-		if ((Row > 0) && (Row <= _Number_of_rows) && (Column > 0) && (Column <= _Number_of_columns))
-		{ 
-			_Position = (Row - 1) * _Number_of_columns;
-
-			_Position += (Column - 1);
-
-			Location = (char*)(&Matrix_array + _Position);
-
-			*Element = *Location;
-		}
-	}
-
-	void set_element(unsigned Row, unsigned Column, unsigned Data)
-	{
-		unsigned _Position = 0;
-		unsigned* Location = nullptr;
-
-		if ((Row > 0) && (Row <= _Number_of_rows) && (Column > 0) && (Column <= _Number_of_columns))
-		{
-			_Position = (Row - 1) * _Number_of_columns;
-
-			_Position += (Column - 1);
-
-			Location = (unsigned*)(&Matrix_array + _Position);
-
-			*Location = Data;
-		}
-	}
-
-	void set_element(unsigned Row, unsigned Column, int Data)
-	{
-		unsigned _Position = 0;
-		int* Location = nullptr;
-
-		if ((Row > 0) && (Row <= _Number_of_rows) && (Column > 0) && (Column <= _Number_of_columns))
-		{
-			_Position = (Row - 1) * _Number_of_columns;
-
-			_Position += (Column - 1);
-
-			Location = (int*)(&Matrix_array + _Position);
-
-			*Location = Data;
-		}
-	}
-
-	void set_element(unsigned Row, unsigned Column, float Data)
-	{
-		unsigned _Position = 0;
-		float* Location = nullptr;
-
-		if ((Row > 0) && (Row <= _Number_of_rows) && (Column > 0) && (Column <= _Number_of_columns))
-		{
-			_Position = (Row - 1) * _Number_of_columns;
-
-			_Position += (Column - 1);
-
-			Location = (float*)(&Matrix_array + _Position);
-
-			*Location = Data;
-		}
-	}
-
-	void set_element(unsigned Row, unsigned Column, char Data)
-	{
-		unsigned _Position = 0;
-		char* Location = nullptr;
-
-		if ((Row > 0) && (Row <= _Number_of_rows) && (Column > 0) && (Column <= _Number_of_columns))
-		{
-			_Position = (Row - 1) * _Number_of_columns;
-				
-			_Position += (Column - 1);
-
-			Location = (char*)(&Matrix_array + _Position);
-
-			*Location = Data;
-		}
-	}
+	OSR_matrix& operator= (OSR_matrix& Matrix_B);
+	OSR_matrix& operator+=(OSR_matrix& Matrix_B);
+	OSR_matrix  operator+ (OSR_matrix& Right);
 
 private:
 	
@@ -271,9 +158,9 @@ private:
 
 	OSR_matrix::Element_types _Matrix_elements_type = BYTE_TYPE;
 
-	unsigned _Matrix_size = 0;
+	unsigned _Matrix_size_in_bytes = 0;
 
-	void* Matrix_array;
+	void* Matrix_array = nullptr;
 
 };
 
