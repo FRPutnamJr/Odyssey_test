5c4ccbb09eb0a6b558cfb4d90f0f7893a09dd872
Frank Putnam, Jr <frankputnam@comcast.net>
2019-06-15 20:34:11 -0400

    Implemented overloaded Transpose

--- Code/OSR_matrix.cpp
+++ Code/OSR_matrix.cpp
@@ -3,7 +3,7 @@
 * Filename              :   OSR_matrix.cpp
 * Author                :   Frank Putnam, Jr.
 * Origin Date           :   2019-06-11
-* Version               :   0.0.3
+* Version               :   0.5
 * Compiler              :   Microsoft Visual Studio 2019 C30 v3.30c
 * Target                :   PC
 * Notes                 :   None
@@ -11,7 +11,12 @@
 /*************** MODULE REVISION LOG ******************************************
 *
 *    Date    Software Version    Initials   Description
-*  2019-06-11       0.1              FRP      Created.
+*  2019/06/11       0.1              FRP      Created.
+*  2019/06/12       0.2              FRP      Implemented constructor, destructor, overloaded get_element, overloaded set_element.
+*  2019/06/13       0.3              FRP      Reworked overloaded get_element, overloaded set_element.
+*                                             Implemented overloaded operator=, overloaded operator+=, overloaded+.
+*  2019/06/14       0.4              FRP      Implemented overloaded operator*= (Scaler Multiplication).
+*  2019/06/15       0.5              FRP      Implemented overloaded Transpose.
 *
 *******************************************************************************/
 /** @file Odeyssey_matrix.cpp
@@ -75,16 +80,13 @@ constexpr auto CONSTANT = 5;
 *  2019/06/12       0.2              FRP      Implemented constructor, destructor, overloaded get_element, overloaded set_element.
 *  2019/06/13       0.3              FRP      Reworked overloaded get_element, overloaded set_element.
 *                                             Implemented overloaded operator=, overloaded operator+=, overloaded+.
+*  2019/06/14       0.4              FRP      Implemented overloaded operator*= (Scaler Multiplication).
+*  2019/06/15       0.5              FRP      Implemented overloaded Transpose.
 *
 *******************************************************************************/
 
 #include "OSR_matrix.h"
 
-
-void OSR_matrix::Transpose(void)
-{
-	;
-}
 
 void OSR_matrix::get_element(unsigned Row, unsigned Column, unsigned* Element)
 {
@@ -234,7 +236,7 @@ void OSR_matrix::set_element(unsigned Row, unsigned Column, char Data)
 OSR_matrix& OSR_matrix::operator=(OSR_matrix& Right_matrix) 
 {
 	unsigned Position      = 0;
-	unsigned Element_count = _Number_of_rows * _Number_of_columns;
+	unsigned Element_count = this->get_number_of_rows() * this->get_number_of_columns();
 
 	switch (_Matrix_elements_type)
 	{
@@ -375,6 +377,233 @@ OSR_matrix& OSR_matrix::operator+=(OSR_matrix& Matrix_B)   // Addition.
 	}
 	return  (*this);
 }
+
+OSR_matrix& OSR_matrix::operator*=(int Scaler_value)   // Scaler multiplication. 
+{
+	OSR_matrix& Scaler_matrix(*this);
+
+	unsigned Position = 0;
+
+	int* The_matrix_array = (int*)Matrix_array;
+	int* Scaler_matrix_array = (int*)Scaler_matrix.Matrix_array;
+
+	for (Position = 0; Position < this->_Number_of_elements; Position++)
+	{
+		*Scaler_matrix_array++ = *The_matrix_array++ * Scaler_value;
+	}
+
+	return  (Scaler_matrix);
+}
+
+
+OSR_matrix& OSR_matrix::operator*= (unsigned Scaler_value)
+{
+	OSR_matrix& Scaler_matrix(*this);
+
+	unsigned Position = 0;
+
+	unsigned* The_matrix_array    = (unsigned*)Matrix_array;
+	unsigned* Scaler_matrix_array = (unsigned*)Scaler_matrix.Matrix_array;
+
+	for (Position = 0; Position < this->_Number_of_elements; Position++)
+	{
+		*Scaler_matrix_array++ = *The_matrix_array++ * Scaler_value;
+	}
+
+	return  (Scaler_matrix);
+
+
+}
+
+
+OSR_matrix& OSR_matrix::operator*= (float Scaler_value)
+{
+	OSR_matrix& Scaler_matrix(*this);
+
+	unsigned Position = 0;
+
+	float* The_matrix_array    = (float*)Matrix_array;
+	float* Scaler_matrix_array = (float*)Scaler_matrix.Matrix_array;
+
+	for (Position = 0; Position < this->_Number_of_elements; Position++)
+	{
+		*Scaler_matrix_array++ = *The_matrix_array++ * Scaler_value;
+	}
+
+	return  (Scaler_matrix);
+}
+
+
+OSR_matrix& OSR_matrix::operator*= (char Scaler_value)
+{
+	OSR_matrix& Scaler_matrix(*this);
+
+	unsigned Position = 0;
+
+	char* The_matrix_array    = (char*)Matrix_array;
+	char* Scaler_matrix_array = (char*)Scaler_matrix.Matrix_array;
+
+	for (Position = 0; Position < this->_Number_of_elements; Position++)
+	{
+		*Scaler_matrix_array++ = *The_matrix_array++ * Scaler_value;
+	}
+
+	return  (Scaler_matrix);
+}
+
+/* 
+   * Create return matrix derived of A_matrix.
+   * Create array of elements to hold results of A_matrix * B_matrix.
+   * Multiply A_matrix_row_elements by B_matrix_column_elements.
+   * Add array of elements into return matrix elements.
+   * Return new matrix.
+*/
+OSR_matrix& OSR_matrix::operator*=(OSR_matrix& Matrix_B)
+{
+	OSR_matrix Matrix_C(*this);				// Create return matrix derived of A_matrix.
+	
+	int* A_matrix_array = (int*)Matrix_array;
+	int* B_matrix_array = (int*)Matrix_B.Matrix_array;
+	int* C_matrix_array = (int*)Matrix_C.Matrix_array;
+
+
+	int* AB_array = new int(_Number_of_elements);			// Create array of elements to hold results of A_matrix * B_matrix.
+
+	unsigned Index = 0;
+	unsigned Position = 0;
+
+	// Multiply A_matrix_row_elements by B_matrix_column_elements.
+	for (int A_row = 0; A_row < _Number_of_rows; A_row++)
+	{
+		B_matrix_array = (int*)Matrix_B.Matrix_array;
+
+		for (int A_column = 0; A_column < _Number_of_columns; A_column++)
+		{
+			for (int B_row = 0; B_row < _Number_of_rows; B_row++)
+			{
+				AB_array[Index++] = (int)(*A_matrix_array * *B_matrix_array++);
+			}
+
+			A_matrix_array++;
+		}
+	}
+
+	// Add array of elements into return matrix elements.
+	*C_matrix_array = 0;
+
+	for (Position = 0; Position < _Number_of_elements; Position++)
+	{
+		Index = Position;
+
+		for (int Ix = 0; Ix < _Number_of_columns; Ix++)
+		{
+			*C_matrix_array += (int)AB_array[Index];
+
+			Index += _Number_of_columns;
+		}
+
+		C_matrix_array++;
+	}
+	
+	return  (Matrix_C);
+}
+
+
+const OSR_matrix OSR_matrix::Transpose(int Dummy_type)						// Transpose a matrix of signed integer elements.
+{
+	const OSR_matrix Transpose_matrix = OSR_matrix(this->get_number_of_columns(), this->get_number_of_rows(), this->get_type_of_element());
+	
+	int* Matrix_array_column_element = (int*)this->Matrix_array;
+	int* Transpose_array_row_element = (int*)Transpose_matrix.Matrix_array;
+
+	unsigned Row    = 0;
+	unsigned Column = 0;
+
+	for (Column=0; Column < Transpose_matrix._Number_of_columns; Column++)
+	{
+		for (Row = 0; Row < Transpose_matrix._Number_of_rows; Row++)
+		{
+			*(Transpose_array_row_element + (Row * Transpose_matrix._Number_of_columns)) = *Matrix_array_column_element++;
+		}
+
+		Transpose_array_row_element++;
+	}
+
+	return  (Transpose_matrix);
+}
+
+
+const OSR_matrix OSR_matrix::Transpose(unsigned Dummy_type)									// Transpose a matrix of unsigned integer elements.
+{
+	const OSR_matrix Transpose_matrix = OSR_matrix(this->get_number_of_columns(), this->get_number_of_rows(), this->get_type_of_element());
+
+	unsigned* Matrix_array_column_element = (unsigned*)Matrix_array;
+	unsigned* Transpose_array_row_element = (unsigned*)Transpose_matrix.Matrix_array;
+
+	unsigned Row = 0;
+	unsigned Column = 0;
+
+	for (Column = 0; Column < Transpose_matrix._Number_of_columns; Column++)
+	{
+		for (Row = 0; Row < Transpose_matrix._Number_of_rows; Row++)
+		{
+			*(Transpose_array_row_element + (Row * Transpose_matrix._Number_of_columns)) = *Matrix_array_column_element++;
+		}
+
+		Transpose_array_row_element++;
+	}
+
+	return  (Transpose_matrix);
+}
+
+const OSR_matrix OSR_matrix::Transpose(float Dummy_type) 	 								// Transpose a matrix of float elements.
+{
+	const OSR_matrix Transpose_matrix = OSR_matrix(this->get_number_of_columns(), this->get_number_of_rows(), this->get_type_of_element());
+
+	float* Matrix_array_column_element = (float*)this->Matrix_array;
+	float* Transpose_array_row_element = (float*)Transpose_matrix.Matrix_array;
+
+	unsigned Row = 0;
+	unsigned Column = 0;
+
+	for (Column = 0; Column < Transpose_matrix._Number_of_columns; Column++)
+	{
+		for (Row = 0; Row < Transpose_matrix._Number_of_rows; Row++)
+		{
+			*(Transpose_array_row_element + (Row * Transpose_matrix._Number_of_columns)) = *Matrix_array_column_element++;
+		}
+
+		Transpose_array_row_element++;
+	}
+
+	return  (Transpose_matrix);
+}
+
+const OSR_matrix OSR_matrix::Transpose(char Dummy_type)										// Transpose a matrix of byte elements.
+{
+	const OSR_matrix Transpose_matrix = OSR_matrix(this->get_number_of_columns(), this->get_number_of_rows(), this->get_type_of_element());
+
+	char* Matrix_array_column_element = (char*)this->Matrix_array;
+	char* Transpose_array_row_element = (char*)Transpose_matrix.Matrix_array;
+
+	unsigned Row = 0;
+	unsigned Column = 0;
+
+	for (Column = 0; Column < Transpose_matrix._Number_of_columns; Column++)
+	{
+		for (Row = 0; Row < Transpose_matrix._Number_of_rows; Row++)
+		{
+			*(Transpose_array_row_element + (Row * Transpose_matrix._Number_of_columns)) = *Matrix_array_column_element++;
+		}
+
+		Transpose_array_row_element++;
+	}
+
+	return  (Transpose_matrix);
+}
+
+
+
 
 OSR_matrix OSR_matrix::operator+(OSR_matrix& Matrix_B)   // Addition. 
 { 
--- Code/OSR_matrix.h
+++ Code/OSR_matrix.h
@@ -3,7 +3,7 @@
 * Filename              :   OSR_matrix.h
 * Author                :   Frank Putnam, Jr.
 * Origin Date           :   2019-06-08
-* Version               :   0.0.3
+* Version               :   0.5
 * Compiler              :   Microsoft Visual Studio 2019 C30 v3.30c
 * Target                :   PC
 * Notes                 :   None
@@ -15,6 +15,8 @@
 *  2019/06/12       0.2              FRP      Implemented constructor, destructor, overloaded get_element, overloaded set_element.
 *  2019/06/13       0.3              FRP      Reworked overloaded get_element, overloaded set_element.
 *                                             Implemented overloaded operator=, overloaded operator+=, overloaded+.
+*  2019/06/14       0.4              FRP      Implemented overloaded operator*= (Scaler Multiplication).
+*  2019/06/15       0.5              FRP      Implemented overloaded Transpose.
 *
 *****************************************************************************/
 /** @file OSR_matrix.h
@@ -45,25 +47,28 @@
 
 
   /******************************************************************************
-  * Macros
-  *******************************************************************************/
-
-
-
-  /******************************************************************************
   * Typedefs
   *******************************************************************************/
 
+  /******************************************************************************
+  * Constructors
+  *******************************************************************************/
 
   /******************************************************************************
   * Variables
+  *******************************************************************************/
+
+  /******************************************************************************
+  * Detructors
   *******************************************************************************/
 
 
   /******************************************************************************
   * Function Prototypes
   *******************************************************************************/
-#ifdef __cplusplus
+
+
+  #ifdef __cplusplus
 extern "C" {
 #endif
 
@@ -73,6 +78,7 @@ class OSR_matrix
 public:
 	typedef enum Element_types { BYTE_TYPE, SINT_TYPE, UINT_TYPE, FLOAT_TYPE } Element_types;
 
+	
 	OSR_matrix()
 		: _Number_of_rows{ 1 }, _Number_of_columns{ 1 }, _Matrix_elements_type{ BYTE_TYPE }, Matrix_array{ new char[1][1] }
 	{}
@@ -123,38 +129,65 @@ public:
 	
 	~OSR_matrix() {}
 
-	unsigned get_number_of_rows(void)    { return _Number_of_rows; }
-	unsigned get_number_of_columns(void) { return _Number_of_columns; }
-	unsigned get_size_in_bytes(void)     { return _Matrix_size_in_bytes; }
+	unsigned get_number_of_rows(void)     { return _Number_of_rows; }
+	unsigned get_number_of_columns(void)  { return _Number_of_columns; }
+	unsigned get_number_of_elements(void) { return _Number_of_elements; }
+	unsigned get_size_in_bytes(void)      { return _Matrix_size_in_bytes; }
+
+	void* get_array(void) { return Matrix_array; }
 
 	OSR_matrix::Element_types get_type_of_element(void) { return _Matrix_elements_type; }
 
-	void Transpose(void);
+	void get_element(unsigned Row, unsigned Column, unsigned* Element);			// Obtain the Unsinged_integer_element from the Unsigned_integer_matrix at Row and Column.
+	
+	void get_element(unsigned Row, unsigned Column, int* Element);				// Obtain the Singed_integer_element from the Signed_integer_matrix at Row and Column.
+	
+	void get_element(unsigned Row, unsigned Column, float* Element);			// Obtain the Float_element from the Float_matrix at Row and Column.
+	
+	void get_element(unsigned Row, unsigned Column, char* Element);				// Obtain the Byte_element from the Byte_matrix at Row and Column.
+	
+	
+	
+	void set_element(unsigned Row, unsigned Column, unsigned Data);				// Store the Unsinged_integer_value into the Unsigned_integer_matrix at Row and Column.
+	
+	void set_element(unsigned Row, unsigned Column, int Data);					// Store the Singed_integer_value into the Signed_integer_matrix at Row and Column.
+	
+	void set_element(unsigned Row, unsigned Column, float Data);				// Store the Float_value into the Float_matrix at Row and Column.
+	
+	void set_element(unsigned Row, unsigned Column, char Data);					// Store the Byte_value into the Byte_matrix at Row and Column.
 
-	void get_element(unsigned Row, unsigned Column, unsigned* Element);
-	
-	void get_element(unsigned Row, unsigned Column, int* Element);
-	
-	void get_element(unsigned Row, unsigned Column, float* Element);
-	
-	void get_element(unsigned Row, unsigned Column, char* Element);
-	
-	void set_element(unsigned Row, unsigned Column, unsigned Data);
-	
-	void set_element(unsigned Row, unsigned Column, int Data);
-	
-	void set_element(unsigned Row, unsigned Column, float Data);
-	
-	void set_element(unsigned Row, unsigned Column, char Data);
+	OSR_matrix& operator=  (OSR_matrix& Matrix_B);								// Matrix_C = Matrix_B
+	OSR_matrix& operator+= (OSR_matrix& Matrix_B);								// Matrix_C = Matrix_C + Matrix_B 
 
-	OSR_matrix& operator= (OSR_matrix& Matrix_B);
-	OSR_matrix& operator+=(OSR_matrix& Matrix_B);
-	OSR_matrix  operator+ (OSR_matrix& Right);
+	OSR_matrix& operator*= (int Scaler_value);									// Signed_integer_matrix   = Signed_integer_matrix * Signed_integer_scaler_value
+	OSR_matrix& operator*= (unsigned Scaler_value);								// Unsigned_integer_matrix = Unsigned_integer_matrix * Unsigned_integer_scaler_value
+	OSR_matrix& operator*= (float Scaler_value);								// Float_matrix = Float_matrix * Float_scaler_value
+	OSR_matrix& operator*= (char Scaler_value);									// Byte_matrix = Byte_matrix * Byte_scaler_value
+
+
+//	Expected Result : A = { a b }	B = { e f } 	C = { ae + bg  af + bh }
+//	                      { c d }       { g h }         { ce + dg  cf + dh }
+//
+//	                      { 1 2 }       { 5 6 }         { 19 22 }
+//	                      { 3 4 }       { 7 8 }         { 43 50 }
+
+	OSR_matrix& operator*= (OSR_matrix& Matrix_B);								// Matrix_C = Matrix_C * Matrix_B 
+
+	const OSR_matrix Transpose(int Dummy_type);									// Transpose a matrix of signed integer elements.
+	
+	const OSR_matrix Transpose(unsigned Dummy_type);							// Transpose a matrix of unsigned integer elements.
+	
+	const OSR_matrix Transpose(float Dummy_type);								// Transpose a matrix of float elements.
+	
+	const OSR_matrix Transpose(char Dummy_type);								// Transpose a matrix of byte elements.
+
+	OSR_matrix  operator+  (OSR_matrix& Right);
 
 private:
 	
-	unsigned _Number_of_rows    = 1;
-	unsigned _Number_of_columns = 1;
+	unsigned _Number_of_rows     = 1;
+	unsigned _Number_of_columns  = 1;
+	unsigned _Number_of_elements = _Number_of_rows * _Number_of_columns;
 
 	OSR_matrix::Element_types _Matrix_elements_type = BYTE_TYPE;
 
